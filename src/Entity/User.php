6ba02363a1a10841467e87d2a0bd6d3ad5e92f15
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2023-02-08 17:20:18 +0100

    add: De l'instance #[ApiResource] et #[UniqueEntity] de base pour l'entité User

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,13 +2,17 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\Table(name: '`user`')]
+#[ApiResource]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
