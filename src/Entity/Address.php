f70a4ffd5fd77d93582d2a5cea0fa4da617e3ed0
Romain Leroy <romain.leroy@etudiant.univ-reims.fr>
2023-02-15 10:55:17 +0100

    Mise en place des CRUD sur Address

--- src/Entity/Address.php
+++ src/Entity/Address.php
@@ -2,30 +2,59 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Put;
 use App\Repository\AddressRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 #[ORM\Entity(repositoryClass: AddressRepository::class)]
+#[ORM\Table(name: '`address`')]
+#[ApiResource(
+    operations: [
+        new Get(),
+        new Put(),
+        new Patch(),
+    ],
+    normalizationContext: ['groups' => ['get_address']]
+)]
+#[Get]
+#[Patch(
+    normalizationContext: ['groups' => ['set_address']],
+    security: 'object.owner == user'
+)]
+#[Put(
+    normalizationContext: ['groups' => ['set_address']],
+    security: 'object.owner == user'
+)]
 class Address
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
+    #[Groups(['get_address', 'set_address'])]
     private ?int $id = null;
 
     #[ORM\Column(length: 50, nullable: true)]
+    #[Groups(['get_address', 'set_address'])]
     private ?string $name = null;
 
     #[ORM\Column(length: 255)]
+    #[Groups(['get_address', 'set_address'])]
     private ?string $ad = null;
 
     #[ORM\Column(length: 5)]
+    #[Groups(['get_address', 'set_address'])]
     private ?string $pc = null;
 
     #[ORM\Column(length: 50)]
+    #[Groups(['get_address', 'set_address'])]
     private ?string $city = null;
 
     #[ORM\ManyToOne(inversedBy: 'adresses')]
+    #[Groups(['get_address', 'set_address'])]
     private ?Client $client = null;
 
     public function getId(): ?int
