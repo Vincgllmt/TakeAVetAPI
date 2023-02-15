56b3594ee982c00814b478929a7da5d138656239
Alexis Udycz <alexis.udycz@etudiant.univ-reims.fr>
2023-02-15 10:56:34 +0100

    add: du test simple pour POST

---
+++ tests/Api/Thread/PostThreadCest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Tests\Api\Thread;
+
+use App\Factory\ClientFactory;
+use App\Tests\Support\ApiTester;
+use Codeception\Util\HttpCode;
+
+class PostThreadCest
+{
+    public function testThreadPost(ApiTester $I): void
+    {
+        $client = ClientFactory::createOne();
+
+        $I->sendPost('/api/threads', [
+            'lib' => 'test',
+            'message' => 'test',
+            'author' => "/api/users/{$client->getId()}",
+        ]);
+
+        $I->seeResponseCodeIs(HttpCode::CREATED);
+        $I->seeResponseIsJson();
+    }
+}
