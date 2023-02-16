16623c6c43842dc701a82948740c22752bdd8369
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2023-02-16 13:25:30 +0100

    migrate: Address vers le nouveau MCD

--- src/Entity/Address.php
+++ src/Entity/Address.php
@@ -7,6 +7,8 @@ use ApiPlatform\Metadata\Get;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Put;
 use App\Repository\AddressRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Serializer\Annotation\Ignore;
@@ -75,6 +77,14 @@ class Address
     #[ORM\ManyToOne(inversedBy: 'adresses')]
     #[Groups(['address:read', 'address:write'])]
     private ?Client $client = null;
+
+    #[ORM\OneToMany(mappedBy: 'location', targetEntity: Appointment::class)]
+    private Collection $appointments;
+
+    public function __construct()
+    {
+        $this->appointments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -145,5 +155,35 @@ class Address
     public function getDisplayName(): string
     {
         return "$this->ad, $this->city, $this->pc ($this->name)";
+    }
+
+    /**
+     * @return Collection<int, Appointment>
+     */
+    public function getAppointments(): Collection
+    {
+        return $this->appointments;
+    }
+
+    public function addAppointment(Appointment $appointment): self
+    {
+        if (!$this->appointments->contains($appointment)) {
+            $this->appointments->add($appointment);
+            $appointment->setLocation($this);
+        }
+
+        return $this;
+    }
+
+    public function removeAppointment(Appointment $appointment): self
+    {
+        if ($this->appointments->removeElement($appointment)) {
+            // set the owning side to null (unless already changed)
+            if ($appointment->getLocation() === $this) {
+                $appointment->setLocation(null);
+            }
+        }
+
+        return $this;
     }
 }
