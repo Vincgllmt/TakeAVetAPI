ea61b32b98f2754746802f9780b84773f905cb8f
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2023-02-16 13:29:16 +0100

    migrate: TypeAppointment vers le nouveau MCD

--- src/DataFixtures/TypeAppointmentFixtures.php
+++ src/DataFixtures/TypeAppointmentFixtures.php
@@ -11,27 +11,27 @@ class TypeAppointmentFixtures extends Fixture
     public function load(ObjectManager $manager): void
     {
         TypeAppointmentFactory::createOne([
-            'libTypeApp' => 'Normal',
+            'name' => 'Normal',
             'duration' => 10,
         ]);
 
         TypeAppointmentFactory::createOne([
-            'libTypeApp' => 'Blessure',
+            'name' => 'Blessure',
             'duration' => 30,
         ]);
 
         TypeAppointmentFactory::createOne([
-            'libTypeApp' => 'Operation (3h)',
+            'name' => 'Operation (3h)',
             'duration' => 180,
         ]);
 
         TypeAppointmentFactory::createOne([
-            'libTypeApp' => 'Operation (2h)',
+            'name' => 'Operation (2h)',
             'duration' => 120,
         ]);
 
         TypeAppointmentFactory::createOne([
-            'libTypeApp' => 'Operation (1h)',
+            'name' => 'Operation (1h)',
             'duration' => 60,
         ]);
 
