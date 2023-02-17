54f3f127ba61d8c9595b3d5bb96c72cdfafd78ed
Clément <clement.perrot@etudiant.univ-reims.fr>
2023-02-17 11:39:12 +0100

    début des tests pour l'entité Animal

--- tests/Api/Animal/GetAnimalsCest.php
+++ tests/Api/Animal/GetAnimalsCest.php
@@ -1,2 +1,18 @@
 <?php
-declare(strict_types=1);
+
+use App\Factory\AnimalFactory;
+use App\Tests\Support\ApiTester;
+
+class GetAnimalCest
+{
+    public function getAllAnimals(ApiTester $I): void
+    {
+        AnimalFactory::createMany(20);
+
+    }
+
+    public function getOneAnimal(ApiTester $I): void
+    {
+
+    }
+}
