a3551b5bb18adfe4195d88be13ba80907b9bfe5b
udyc0001 <nxtuto55@gmail.com>
2023-02-19 17:58:29 +0100

    add: Fixture des types d'animaux qu'un véto accepte

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,35 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\TypeAnimalFactory;
+use App\Factory\VetoFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class AppFixtures extends Fixture
+class AppFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        // Populate the accepting animals type for each vet
+        foreach (VetoFactory::repository()->findAll() as $veto) {
+            foreach (TypeAnimalFactory::randomRange(0, 10) as $typeAnimal) {
+                $veto->addAccepting($typeAnimal->object());
+            }
+        }
 
         $manager->flush();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getDependencies(): array
+    {
+        return [
+            AnimalFixtures::class,
+            VetoFixtures::class,
+            TypeAnimalFixtures::class,
+        ];
     }
 }
