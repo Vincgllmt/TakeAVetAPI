c023c488d74568ce6bd2fc0611154af58bba4b77
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2023-02-22 21:47:20 +0100

    add: d'un listener pour automatiquement définir le vétérinaire

---
+++ src/EntityListener/AgendaListener.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\EntityListener;
+
+use App\Entity\Agenda;
+use App\Entity\Veto;
+use Doctrine\Bundle\DoctrineBundle\Attribute\AsEntityListener;
+use Doctrine\ORM\Events;
+use Symfony\Component\Security\Core\Security;
+
+#[AsEntityListener(
+    event: Events::prePersist,
+    entity: Agenda::class
+)]
+class AgendaListener
+{
+    private Security $security;
+
+    public function __construct(Security $security)
+    {
+        $this->security = $security;
+    }
+
+    public function prePersist(Agenda $agenda): void
+    {
+        $user = $this->security->getUser();
+
+        if ($user instanceof Veto) {
+            $agenda->setVeto($user);
+        }
+    }
+}
