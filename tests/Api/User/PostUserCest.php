018c12266ee7357df7db6560255de08b390729c9
Alexis Udycz <alexis.udycz@etudiant.univ-reims.fr>
2023-03-01 10:51:05 +0100

    add: du test testCreateVeto et modif du expectedPropertiesResultPost : OK

--- tests/Api/User/PostUserCest.php
+++ tests/Api/User/PostUserCest.php
@@ -6,20 +6,25 @@ use App\Tests\Support\ApiTester;
 
 class PostUserCest
 {
-    protected static function expectedPropertiesResultPost(): array
+    protected static function expectedPropertiesResultPost(bool $isClient): array
     {
-        return [
+        $properties = [
             'id' => 'integer',
-            'isHusbandry' => 'boolean',
             'email' => 'string',
             'lastName' => 'string',
             'firstName' => 'string',
             'phone' => 'string|null',
             'avatarPath' => 'string|null',
         ];
+
+        if ($isClient) {
+            $properties['isHusbandry'] = 'boolean';
+        }
+
+        return $properties;
     }
 
-    public function testCreateUser(ApiTester $I): void
+    public function testCreateClient(ApiTester $I): void
     {
         $I->sendPost('/api/register', [
             'email' => 'test@test.test',
@@ -30,6 +35,20 @@ class PostUserCest
 
         $I->seeResponseCodeIsSuccessful();
         $I->seeResponseIsJson();
-        $I->seeResponseIsAnItem(self::expectedPropertiesResultPost());
+        $I->seeResponseIsAnItem(self::expectedPropertiesResultPost(true));
+    }
+
+    public function testCreateVeto(ApiTester $I): void
+    {
+        $I->sendPost('/api/vet/register', [
+            'email' => 'test@test.test',
+            'password' => 'password',
+            'lastName' => 'Doe',
+            'firstName' => 'John',
+        ]);
+
+        $I->seeResponseCodeIsSuccessful();
+        $I->seeResponseIsJson();
+        $I->seeResponseIsAnItem(self::expectedPropertiesResultPost(false));
     }
 }
