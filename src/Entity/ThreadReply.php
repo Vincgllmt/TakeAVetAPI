ff1574ffb12aabb3340f31256608c1bc63ffe2df
Guillemot Vincent <vincent.guillemot@etudiant.univ-reims.fr>
2023-03-01 10:31:13 +0100

    Ajout documentation pour la méthode delete

--- src/Entity/ThreadReply.php
+++ src/Entity/ThreadReply.php
@@ -23,6 +23,7 @@ use Symfony\Component\Serializer\Annotation\Groups;
         ),
         new GetCollection(openapiContext: ['summary' => 'Get all reply']),
         new Delete(
+            openapiContext: ['summary' => 'delete a reply in a thread'],
             security: "is_granted('IS_AUTHENTICATED_FULLY') and object.user == user"
         ),
          new Post(
