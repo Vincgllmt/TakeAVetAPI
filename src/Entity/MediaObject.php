250730feba3c3830a8ecd94f25134178de05a703
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2023-03-02 18:47:31 +0100

    add: de l'entité MediaObject, qui représente un media sur le serveur

---
+++ src/Entity/MediaObject.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Metadata\ApiProperty;
+use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use App\Controller\CreateMediaObjectAction;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints\NotNull;
+use Vich\UploaderBundle\Mapping\Annotation\Uploadable;
+use Vich\UploaderBundle\Mapping\Annotation\UploadableField;
+
+/**
+ * @see https://api-platform.com/docs/core/file-upload/
+ */
+#[Uploadable]
+#[ORM\Entity]
+#[ApiResource(
+    types: ['https://schema.org/MediaObject'],
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(
+            controller: CreateMediaObjectAction::class,
+            openapiContext: [
+                "requestBody" => [
+                    "content" => [
+                        "multipart/form-data" => [
+                            "schema" => [
+                                "type" => "object",
+                                "properties" => [
+                                    "file" => [
+                                        "type" => "string",
+                                        "format" => "binary"
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ],
+            validationContext: ['groups' => ['Default', 'media_object_create']],
+            deserialize: false
+        )
+    ],
+    normalizationContext: ['groups' => ['media_object:read']]
+)]
+class MediaObject
+{
+    #[ORM\Id, ORM\Column, ORM\GeneratedValue]
+    private ?int $id = null;
+
+    #[ApiProperty(types: ['https://schema.org/contentUrl'])]
+    #[Groups(['media_object:read'])]
+    public ?string $contentUrl = null;
+
+    #[UploadableField(mapping: "media_object", fileNameProperty: "filePath")]
+    #[NotNull(groups: ['media_object_create'])]
+    public ?File $file = null;
+
+    #[ORM\Column(nullable: true)]
+    public ?string $filePath = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+}
