455663ee18e6d69dfd05f426760c2bb9b6dc7565
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2023-03-07 22:52:51 +0100

    fix: des tests exceptedThreadProperties, anyoneCanGetThreads : OK

--- tests/Api/Thread/GetThreadCest.php
+++ tests/Api/Thread/GetThreadCest.php
@@ -2,31 +2,61 @@
 
 namespace App\Tests\Api\Thread;
 
+use App\Entity\Thread;
+use App\Factory\ClientFactory;
 use App\Factory\ThreadFactory;
 use App\Tests\Support\ApiTester;
 
 class GetThreadCest
 {
-    public function getAllThread(ApiTester $I): void
+    private static function exceptedThreadProperties(bool $isFull): array
     {
-        ThreadFactory::createMany(2);
+        $properties = [
+            'id' => 'integer',
+            'subject' => 'string',
+            'description' => 'string',
+            'createdAt' => 'string',
+            'author' => 'array|null',
+            'resolved' => 'boolean',
+        ];
+
+        if ($isFull) {
+            $properties['replies'] = 'array';
+        }
+
+        return $properties;
+    }
+
+    public function anyoneCanGetThreads(ApiTester $I): void
+    {
+        ThreadFactory::createMany(2, [
+            'author' => ClientFactory::createOne(),
+        ]);
 
         $I->sendGet('/api/threads');
 
         $I->seeResponseCodeIs(200);
         $I->seeResponseIsJson();
+        $I->seeResponseIsACollection(Thread::class, '/api/threads');
         $I->seeResponseMatchesJsonType([
             'hydra:totalItems' => 'integer',
+            'hydra:member' => [
+                self::exceptedThreadProperties(false),
+                self::exceptedThreadProperties(false),
+            ],
         ]);
     }
 
-    public function getOneThread(ApiTester $I): void
+    public function anyoneCanGetOneThreadWithHisId(ApiTester $I): void
     {
-        $thread = ThreadFactory::createOne();
+        $thread = ThreadFactory::createOne([
+            'author' => ClientFactory::createOne(),
+        ]);
 
         $I->sendGet("/api/threads/{$thread->getId()}");
 
         $I->seeResponseCodeIs(200);
         $I->seeResponseIsJson();
+        $I->seeResponseIsAnItem(self::exceptedThreadProperties(true));
     }
 }
