9a45228a52b4da788bb80efefb33ad64b9bd6770
Vincent Guillemot <vincent.guillemot@etudiant.univ-reims.fr>
2023-03-09 17:20:14 +0100

    Ajout du test pour la collection de vaccine

--- tests/Api/Vaccine/GetVaccineCest.php
+++ tests/Api/Vaccine/GetVaccineCest.php
@@ -2,7 +2,21 @@
 
 namespace App\Tests\Api\Vaccine;
 
+use App\Factory\VaccineFactory;
+use App\Tests\Support\ApiTester;
+
 class GetVaccineCest
 {
+    public function getAllVaccine(ApiTester $I): void
+    {
+        VaccineFactory::createMany(2);
 
+        $I->sendGet('/api/threads');
+
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseMatchesJsonType([
+            'hydra:totalItems' => 'integer',
+        ]);
+    }
 }
