6430c95f5fc7e627544a69b85219f7ab4e165e2d
Vincent Guillemot <vincent.guillemot@etudiant.univ-reims.fr>
2023-03-09 17:48:12 +0100

    Ajout du test vérifiant qu'on peut patch

--- tests/Api/Vaccine/PatchVaccineCest.php
+++ tests/Api/Vaccine/PatchVaccineCest.php
@@ -2,8 +2,10 @@
 
 namespace App\Tests\Api\Vaccine;
 
+use App\Entity\Vaccine;
 use App\Factory\ClientFactory;
 use App\Factory\VaccineFactory;
+use App\Factory\VetoFactory;
 use App\Tests\Support\ApiTester;
 use Codeception\Util\HttpCode;
 
@@ -28,5 +30,18 @@ class PatchVaccineCest
         ]);
         $I->seeResponseCodeIs(HttpCode::FORBIDDEN);
         $I->seeResponseIsJson();
+    }
+    public function patchVaccine(ApiTester $I): void
+    {
+        $veto = VetoFactory::createOne();
+        $vaccine = VaccineFactory::createOne();
+        $I->amLoggedInAs($veto->object());
+        $I->sendPatch("/api/vaccines/{$vaccine->getId()}", [
+            'name' => 'test',
+        ]);
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseIsAnEntity(Vaccine::class, "/api/vaccines/{$vaccine->getId()}");
+        $I->assertEquals('test', $vaccine->getName());
     }
 }
