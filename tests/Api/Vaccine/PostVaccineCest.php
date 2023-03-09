1b36fd712e63b51f6e91b752d7576e8abc6a5549
Vincent Guillemot <vincent.guillemot@etudiant.univ-reims.fr>
2023-03-09 17:26:32 +0100

    Ajout du test de Post pour vaccine

--- tests/Api/Vaccine/PostVaccineCest.php
+++ tests/Api/Vaccine/PostVaccineCest.php
@@ -2,7 +2,24 @@
 
 namespace App\Tests\Api\Vaccine;
 
+use App\Factory\AnimalFactory;
+use App\Factory\VetoFactory;
+use App\Tests\Support\ApiTester;
+use Codeception\Util\HttpCode;
+
 class PostVaccineCest
 {
-
+    public function testVaccinePost(ApiTester $I): void
+    {
+        $veto = VetoFactory::createOne();
+        $animal = AnimalFactory::createOne();
+        $I->sendPost('/api/vaccines', [
+            'name' => 'test',
+            'next' => '2023-03-09T16:24:03.030Z',
+            'last' => '2023-03-09T16:24:03.030Z',
+            'animal' => "/api/animals/{$animal->getId()}",
+        ]);
+        $I->seeResponseCodeIs(HttpCode::CREATED);
+        $I->seeResponseIsJson();
+    }
 }
