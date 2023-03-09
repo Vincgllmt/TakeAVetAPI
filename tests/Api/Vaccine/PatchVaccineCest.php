1384a18712cd1eb60b0289805404568d21ab78a5
Vincent Guillemot <vincent.guillemot@etudiant.univ-reims.fr>
2023-03-09 17:44:53 +0100

    Ajout du test vérifiant qu'on ne peut pas modifier un vaccin si on n'est pas connecté

--- tests/Api/Vaccine/PatchVaccineCest.php
+++ tests/Api/Vaccine/PatchVaccineCest.php
@@ -2,7 +2,19 @@
 
 namespace App\Tests\Api\Vaccine;
 
+use App\Factory\VaccineFactory;
+use App\Tests\Support\ApiTester;
+use Codeception\Util\HttpCode;
+
 class PatchVaccineCest
 {
-
+    public function cantPatchVaccineUnauthenticated(ApiTester $I): void
+    {
+        $vaccine = VaccineFactory::createOne();
+        $I->sendPatch("/api/vaccines/{$vaccine->getId()}", [
+            'name' => 'test',
+        ]);
+        $I->seeResponseCodeIs(HttpCode::UNAUTHORIZED);
+        $I->seeResponseIsJson();
+    }
 }
