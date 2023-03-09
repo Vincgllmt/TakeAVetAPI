20373a55d3caa9faafc300be8e7b1272055dafb7
Vincent Guillemot <vincent.guillemot@etudiant.univ-reims.fr>
2023-03-09 17:30:29 +0100

    Ajout du test de delete pour vérifier qu'un véto peut supprimer un vaccin

--- tests/Api/Vaccine/DeleteVaccineCest.php
+++ tests/Api/Vaccine/DeleteVaccineCest.php
@@ -3,6 +3,7 @@
 namespace App\Tests\Api\Vaccine;
 
 use App\Factory\VaccineFactory;
+use App\Factory\VetoFactory;
 use App\Tests\Support\ApiTester;
 
 class DeleteVaccineCest
@@ -10,7 +11,16 @@ class DeleteVaccineCest
     public function anonymousVetoForbiddenToDelete(ApiTester $I): void
     {
         $vaccine = VaccineFactory::createOne();
-        $I->sendDelete("/api/thread_replies/{$vaccine->getId()}");
+        $I->sendDelete("/api/vaccines/{$vaccine->getId()}");
         $I->seeResponseCodeIs(401);
+    }
+
+    public function VetoCanDeleteVaccine(ApiTester $I): void
+    {
+        $user = VetoFactory::createOne();
+        $vaccine = VaccineFactory::createOne();
+        $I->amLoggedInAs($user->object());
+        $I->sendDelete("/api/vaccines/{$vaccine->getId()}");
+        $I->seeResponseCodeIs(204);
     }
 }
