25ec8fcc518edbb89379663b3ca3d593a99d0ea8
Vincent Guillemot <vincent.guillemot@etudiant.univ-reims.fr>
2023-03-09 17:28:25 +0100

    Ajout du test de delete pour vérifier qu'il faut être connecté pour delete

--- tests/Api/Vaccine/DeleteVaccineCest.php
+++ tests/Api/Vaccine/DeleteVaccineCest.php
@@ -2,7 +2,15 @@
 
 namespace App\Tests\Api\Vaccine;
 
+use App\Factory\VaccineFactory;
+use App\Tests\Support\ApiTester;
+
 class DeleteVaccineCest
 {
-
+    public function anonymousVetoForbiddenToDelete(ApiTester $I): void
+    {
+        $vaccine = VaccineFactory::createOne();
+        $I->sendDelete("/api/thread_replies/{$vaccine->getId()}");
+        $I->seeResponseCodeIs(401);
+    }
 }
