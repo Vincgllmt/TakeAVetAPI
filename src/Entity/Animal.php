21788ba60bc3d0caac7789679a29b3b530e982f6
Vincent Guillemot <vincent.guillemot@etudiant.univ-reims.fr>
2023-03-13 20:01:48 +0100

    Ajout de l'opération getCollection dans Animal.php

--- src/Entity/Animal.php
+++ src/Entity/Animal.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
 use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
 use ApiPlatform\Metadata\Patch;
 use ApiPlatform\Metadata\Post;
 use ApiPlatform\Metadata\Put;
@@ -24,6 +25,11 @@ use Symfony\Component\Serializer\Annotation\Groups;
             ],
             normalizationContext: ['groups' => ['animal:read']],
             security: 'object.owner == user'
+        ),
+        new GetCollection(
+            openapiContext: [
+                'summary' => 'Get all animals'
+            ]
         ),
         new Post(
             openapiContext: [
