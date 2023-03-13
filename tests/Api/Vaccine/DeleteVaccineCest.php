dba53fc6da32c666e8868779497b00f9987d2b83
Vincent Guillemot <vincent.guillemot@etudiant.univ-reims.fr>
2023-03-13 20:09:43 +0100

    Ajout des tests pour l'opé delete des vaccins

---
+++ tests/Api/Vaccine/DeleteVaccineCest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Api\Vaccine;
+
+use App\Factory\AnimalFactory;
+use App\Factory\ClientFactory;
+use App\Factory\VaccineFactory;
+use App\Factory\VetoFactory;
+use App\Tests\Support\ApiTester;
+use Codeception\Util\HttpCode;
+
+class DeleteVaccineCest
+{
+    public function anonymousVetoForbiddenToDelete(ApiTester $I): void
+    {
+        $animal = AnimalFactory::createOne();
+        $vaccine = VaccineFactory::createOne(['animal' => $animal]);
+        $I->sendDelete("/api/vaccines/{$vaccine->getId()}");
+        $I->seeResponseCodeIs(401);
+    }
+
+    public function VetoCanDeleteVaccine(ApiTester $I): void
+    {
+        $user = VetoFactory::createOne();
+        $animal = AnimalFactory::createOne();
+        $vaccine = VaccineFactory::createOne(['animal' => $animal]);
+        $I->amLoggedInAs($user->object());
+        $I->sendDelete("/api/vaccines/{$vaccine->getId()}");
+        $I->seeResponseCodeIs(204);
+    }
+    public function ClientForbiddenToDeleteVaccine(ApiTester $I): void
+    {
+        $user = ClientFactory::createOne();
+        $animal = AnimalFactory::createOne();
+        $vaccine = VaccineFactory::createOne(['animal' => $animal]);
+        $I->amLoggedInAs($user->object());
+        $I->sendDelete("/api/vaccines/{$vaccine->getId()}");
+        $I->seeResponseCodeIs(httpCode::FORBIDDEN);
+    }
+}
