7ffb9e6e35714321c079d4ea4d91d55ec90e3d06
Vincent Guillemot <vincent.guillemot@etudiant.univ-reims.fr>
2023-03-13 18:16:56 +0100

    Ajout des tests liés a l'opération get et getCollection

---
+++ tests/Api/Animal/GetAnimalCest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Tests\Api\Animal;
+
+use App\Factory\AnimalFactory;
+use App\Tests\Support\ApiTester;
+
+class GetAnimalCest
+{
+    public function getAllAnimal(ApiTester $I): void
+    {
+        $animal = AnimalFactory::createMany(5);
+        $I->sendGet('/api/animals');
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+        $I->seeResponseMatchesJsonType([
+            'hydra:totalItems' => 'integer',
+        ]);
+    }
+    public function getOneAnimal(ApiTester $I): void
+    {
+        $animal = AnimalFactory::createOne();
+        $I->sendGet("/api/animals/{$animal->getId()}");
+        $I->seeResponseCodeIs(200);
+        $I->seeResponseIsJson();
+    }
+}
