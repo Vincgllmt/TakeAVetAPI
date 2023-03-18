80d287aa02c14c5d1a9f970523d003eabab73f12
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2023-03-18 19:28:38 +0100

    add: de la classe abstract du type de formulaire pour la création d'un utilisateur

---
+++ src/Form/RegistrationFormUserType.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\IsTrue;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+
+abstract class RegistrationFormUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'required' => true,
+                'attr' => [
+                    'placeholder' => 'Votre adresse email',
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('lastName', TextType::class, [
+                'required' => true,
+                'label' => 'Nom',
+                'attr' => [
+                    'placeholder' => 'Votre nom',
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('firstName', TextType::class, [
+                'required' => true,
+                'label' => 'Prénom',
+                'attr' => [
+                    'placeholder' => 'Votre prénom',
+                    'class' => 'form-control',
+                ],
+            ])
+            ->add('agreeTerms', CheckboxType::class, [
+                'mapped' => false,
+                'label' => "Accepter les conditions d'utilisation Take'A'Vet.",
+                'constraints' => [
+                    new IsTrue([
+                        'message' => "Vous devez accepter nos conditions d'utilisation de nos services.",
+                    ]),
+                ],
+                'attr' => [
+                    'class' => 'form-check-input',
+                ],
+            ])
+            ->add('plainPassword', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit comporter au moins {{ limit }} caractères.',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+                'attr' => [
+                    'placeholder' => 'Votre mot de passe',
+                    'class' => 'form-control',
+                    'autocomplete' => 'new-password',
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'attr' => [
+                'class' => 'login-form',
+            ],
+            'label_attr' => [
+                'class' => 'form-label',
+            ],
+        ]);
+    }
+}
