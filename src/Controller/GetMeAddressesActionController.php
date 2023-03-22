e4e6f78e6a46bab1211a456528005b5d1fd623f6
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2023-03-22 20:23:27 +0100

    add: du controlleur GetMeAddressesActionController qui permet de récupérer les addresses du client.

---
+++ src/Controller/GetMeAddressesActionController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Client;
+use App\Repository\AddressRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\Exception\AccessDeniedException;
+
+class GetMeAddressesActionController extends AbstractController
+{
+    public function __invoke(AddressRepository $addressRepository): array
+    {
+        $user = $this->getUser();
+
+        if (!($user instanceof Client)) {
+            throw $this->createAccessDeniedException('Only clients can access their addresses.');
+        }
+
+        return $addressRepository->findBy(['client' => $user]);
+    }
+}
