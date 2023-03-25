a3bc6ce99244e50d0df98f25ef57af1ad47ec7e1
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2023-03-25 17:47:31 +0100

    add: du profile (après longgggtemps...)

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
