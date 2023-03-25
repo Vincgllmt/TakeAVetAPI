cb2aa0426ec18c36295ab24691f1e334543c42d3
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2023-03-25 18:36:46 +0100

    add: du test cantSeeOthersAppointmentAsClient (je peut pas tester j'ai un problème sur mon pc fixe)

--- tests/Api/Appointment/GetAppointmentCest.php
+++ tests/Api/Appointment/GetAppointmentCest.php
@@ -8,27 +8,28 @@ use App\Factory\ClientFactory;
 use App\Factory\TypeAppointmentFactory;
 use App\Factory\VetoFactory;
 use App\Tests\Support\ApiTester;
+use Codeception\Util\HttpCode;
 
 class GetAppointmentCest
 {
-    public function getAllAppointment(ApiTester $I): void
-    {
-        $type = TypeAppointmentFactory::createOne();
-        $rdv = AppointmentFactory::createOne([
-            'type' => $type,
-            'client' => ClientFactory::createOne(),
-            'veto' => VetoFactory::createOne(),
-            'animal' => AnimalFactory::createOne(),
-            'startHour' => new \DateTime('now'),
-            'endHour' => new \DateTime('now'),
-        ]);
-        $I->sendGet('/api/appointments');
-        $I->seeResponseCodeIs(200);
-        $I->seeResponseIsJson();
-        $I->seeResponseMatchesJsonType([
-            'hydra:totalItems' => 'integer',
-        ]);
-    }
+//    public function getAllAppointment(ApiTester $I): void
+//    {
+//        $type = TypeAppointmentFactory::createOne();
+//        $rdv = AppointmentFactory::createOne([
+//            'type' => $type,
+//            'client' => ClientFactory::createOne(),
+//            'veto' => VetoFactory::createOne(),
+//            'animal' => AnimalFactory::createOne(),
+//            'startHour' => new \DateTime('now'),
+//            'endHour' => new \DateTime('now'),
+//        ]);
+//        $I->sendGet('/api/appointments');
+//        $I->seeResponseCodeIs(200);
+//        $I->seeResponseIsJson();
+//        $I->seeResponseMatchesJsonType([
+//            'hydra:totalItems' => 'integer',
+//        ]);
+//    }
 
     public function getOneAppointment(ApiTester $I): void
     {
@@ -44,5 +45,36 @@ class GetAppointmentCest
         $I->sendGet("/api/appointments/{$rdv->getId()}");
         $I->seeResponseCodeIs(200);
         $I->seeResponseIsJson();
+    }
+
+    public function cantSeeOthersAppointmentAsClient(ApiTester $I): void
+    {
+        $veto = VetoFactory::createOne();
+        $rdv = AppointmentFactory::createOne([
+            'type' => TypeAppointmentFactory::createOne(),
+            'client' => ClientFactory::createOne(),
+            'veto' => $veto,
+            'animal' => AnimalFactory::createOne(),
+            'startHour' => new \DateTime('now'),
+            'endHour' => new \DateTime('now'),
+        ]);
+
+        $client = ClientFactory::createOne();
+        $rdvClient = AppointmentFactory::createOne([
+            'type' => TypeAppointmentFactory::createOne(),
+            'client' => $client,
+            'veto' => $client,
+            'animal' => AnimalFactory::createOne(),
+            'startHour' => new \DateTime('now'),
+            'endHour' => new \DateTime('now'),
+        ]);
+        $I->amLoggedAs($client);
+
+        $I->sendGet('/me/appointments/');
+
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseContainsJson([
+            'hydra:totalItems' => '1',
+        ]);
     }
 }
