213ae20759ecc8f4423ce921ef021360b0c109c0
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2023-03-25 18:30:58 +0100

    add: de l'action pour la route '/me/appointments'

---
+++ src/Controller/GetMeAppointmentsAction.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Client;
+use App\Repository\AddressRepository;
+use App\Repository\AppointmentRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class GetMeAppointmentsAction extends AbstractController
+{
+    public function __invoke(AppointmentRepository $appointmentRepository): array
+    {
+        $user = $this->getUser();
+
+        if (!($user instanceof Client)) {
+            throw $this->createAccessDeniedException('Only clients can get his appointments');
+        }
+
+        return $appointmentRepository->findBy(['client' => $user]);
+    }
+}
