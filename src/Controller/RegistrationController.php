f52eec3e7049e5e47f956543ee497da11e058e6e
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2023-04-03 23:06:22 +0200

    add: de la route /auth/register/vet

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Client;
+use App\Entity\Veto;
 use App\Form\RegistrationFormClientType;
+use App\Form\RegistrationFormVetoType;
 use App\Security\UserAuthenticator;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -26,6 +28,46 @@ class RegistrationController extends AbstractController
         }
 
         $form = $this->createForm(RegistrationFormClientType::class, $user, [
+            'action' => $redirectQuery,
+        ]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // encode the plain password
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('plainPassword')->getData()
+                )
+            );
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $userAuthenticator->authenticateUser(
+                $user,
+                $authenticator,
+                $request
+            );
+        }
+
+        return $this->render('security/register.html.twig', [
+            'registrationForm' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/auth/register/vet', name: 'app_register_vet')]
+    public function registerVet(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserAuthenticatorInterface $userAuthenticator, UserAuthenticator $authenticator, EntityManagerInterface $entityManager): Response
+    {
+        $user = new Veto();
+
+        $redirectQuery = '';
+        if ($request->query->has('redirect')) {
+            $redirectQuery = '?redirect='.urldecode($request->query->get('redirect'));
+        }
+
+        $form = $this->createForm(RegistrationFormVetoType::class, $user, [
             'action' => $redirectQuery,
         ]);
         $form->handleRequest($request);
